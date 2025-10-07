a30eae03f37bbee97719c74f47667e46bc5bdf0c
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-10-07 11:09:56 +0200

    brighten bg color

--- init.lua
+++ init.lua
@@ -5,14 +5,14 @@ vim.cmd("set shiftwidth=4")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+         "git",
+         "clone",
+         "--filter=blob:none",
+         "https://github.com/folke/lazy.nvim.git",
+         "--branch=stable",
+         lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,4 +21,11 @@ local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+require("onedarkpro").setup({
+    colors = {
+        bg = "#111111",
+    }
+})
+
 vim.cmd("colorscheme onedark_dark")
+
