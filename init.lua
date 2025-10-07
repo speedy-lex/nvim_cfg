0e5be93ed564ccf23851f7715e609b243ae68d43
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-10-07 10:03:32 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {{"olimorris/onedarkpro.nvim", priority = 1000}}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+vim.cmd("colorscheme onedark_dark")
